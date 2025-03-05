4ee7bfa9243075d505617b70ff717f776ec5764e
tugtekinmelis <145451246+tugtekinmelis@users.noreply.github.com>
2025-03-05 15:29:01 +0300

    Add files via upload

---
+++ globaldegisken/global degisken-777/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace global_degisken_777
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTopla = new System.Windows.Forms.Button();
+            this.lblBakiye = new System.Windows.Forms.Label();
+            this.btnOde = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTopla
+            // 
+            this.btnTopla.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTopla.Location = new System.Drawing.Point(12, 37);
+            this.btnTopla.Name = "btnTopla";
+            this.btnTopla.Size = new System.Drawing.Size(138, 91);
+            this.btnTopla.TabIndex = 0;
+            this.btnTopla.Text = "topla";
+            this.btnTopla.UseVisualStyleBackColor = true;
+            this.btnTopla.Click += new System.EventHandler(this.btnTopla_Click);
+            // 
+            // lblBakiye
+            // 
+            this.lblBakiye.AutoSize = true;
+            this.lblBakiye.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBakiye.Location = new System.Drawing.Point(12, 177);
+            this.lblBakiye.Name = "lblBakiye";
+            this.lblBakiye.Size = new System.Drawing.Size(106, 37);
+            this.lblBakiye.TabIndex = 1;
+            this.lblBakiye.Text = "label1";
+            // 
+            // btnOde
+            // 
+            this.btnOde.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOde.Location = new System.Drawing.Point(206, 37);
+            this.btnOde.Name = "btnOde";
+            this.btnOde.Size = new System.Drawing.Size(144, 91);
+            this.btnOde.TabIndex = 2;
+            this.btnOde.Text = "öde";
+            this.btnOde.UseVisualStyleBackColor = true;
+            this.btnOde.Click += new System.EventHandler(this.btnOde_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 403);
+            this.Controls.Add(this.btnOde);
+            this.Controls.Add(this.lblBakiye);
+            this.Controls.Add(this.btnTopla);
+            this.Name = "Form1";
+            this.Text = " Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTopla;
+        private System.Windows.Forms.Label lblBakiye;
+        private System.Windows.Forms.Button btnOde;
+    }
+}
+
---
+++ globaldegisken/global degisken-777/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace global_degisken_777
+{
+    public partial class Form1 : Form
+    {
+        //bakiye global değişkendir.
+        double bakiye = 200;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblBakiye.Text = "Bakiye:" +bakiye.ToString();
+            lblBakiye.ForeColor = Color.Green;
+        }
+
+        private void btnTopla_Click(object sender, EventArgs e)
+        {
+            bakiye = bakiye + 50;
+            lblBakiye.Text = "Bakiye:"  +bakiye.ToString();
+            lblBakiye.ForeColor = Color.Green;
+        }
+
+        private void btnOde_Click(object sender, EventArgs e)
+        {
+            if(bakiye >= 8)
+            {
+                bakiye = bakiye - 8;
+                lblBakiye.Text = "Öde:" + bakiye.ToString();
+            }
+            else
+            {
+                lblBakiye.Text = "FAKİR KAYBOL..";
+                lblBakiye.ForeColor = Color.Red;
+            } 
+        }
+    }
+}
