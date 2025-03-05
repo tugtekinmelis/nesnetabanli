b76751d1967c4ff17359a347f2539a2ee4e6ed66
tugtekinmelis <145451246+tugtekinmelis@users.noreply.github.com>
2025-03-05 15:18:01 +0300

    Add files via upload

---
+++ bolum1uygulama5/bolum1-uygulama5/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bolum1_uygulama7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            lboxMeyveler.Items.Add(txtMeyve.Text);
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            lboxMeyveler.Items.Clear();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            lboxMeyveler.Items.Remove(lboxMeyveler.SelectedItem);
+        }
+    }
+}
