832e501fa4dbde116e4056dc0c43eab9d483cccf
tugtekinmelis <145451246+tugtekinmelis@users.noreply.github.com>
2025-03-05 15:15:56 +0300

    Add files via upload

---
+++ basithesapmakinesi/basit hesap makinesi/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace basit_hesap_makinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.btnCarp = new System.Windows.Forms.Button();
+            this.btnBol = new System.Windows.Forms.Button();
+            this.btnTopla = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.BackColor = System.Drawing.Color.SeaShell;
+            this.txtSayi1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSayi1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtSayi1.Location = new System.Drawing.Point(35, 28);
+            this.txtSayi1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtSayi1.Multiline = true;
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(116, 29);
+            this.txtSayi1.TabIndex = 0;
+            this.txtSayi1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.BackColor = System.Drawing.Color.SeaShell;
+            this.txtSayi2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSayi2.Location = new System.Drawing.Point(170, 28);
+            this.txtSayi2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtSayi2.Multiline = true;
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(116, 29);
+            this.txtSayi2.TabIndex = 1;
+            this.txtSayi2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(31, 80);
+            this.lblSonuc.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(52, 16);
+            this.lblSonuc.TabIndex = 2;
+            this.lblSonuc.Text = "sonuc:";
+            // 
+            // btnCarp
+            // 
+            this.btnCarp.BackColor = System.Drawing.Color.Transparent;
+            this.btnCarp.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCarp.Location = new System.Drawing.Point(35, 120);
+            this.btnCarp.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnCarp.Name = "btnCarp";
+            this.btnCarp.Size = new System.Drawing.Size(117, 33);
+            this.btnCarp.TabIndex = 3;
+            this.btnCarp.Text = "*";
+            this.btnCarp.UseVisualStyleBackColor = false;
+            this.btnCarp.Click += new System.EventHandler(this.btnCarp_Click);
+            // 
+            // btnBol
+            // 
+            this.btnBol.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBol.Location = new System.Drawing.Point(170, 120);
+            this.btnBol.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnBol.Name = "btnBol";
+            this.btnBol.Size = new System.Drawing.Size(117, 33);
+            this.btnBol.TabIndex = 4;
+            this.btnBol.Text = "/";
+            this.btnBol.UseVisualStyleBackColor = true;
+            this.btnBol.Click += new System.EventHandler(this.btnBol_Click);
+            // 
+            // btnTopla
+            // 
+            this.btnTopla.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTopla.Location = new System.Drawing.Point(35, 185);
+            this.btnTopla.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnTopla.Name = "btnTopla";
+            this.btnTopla.Size = new System.Drawing.Size(117, 33);
+            this.btnTopla.TabIndex = 5;
+            this.btnTopla.Text = "+";
+            this.btnTopla.UseVisualStyleBackColor = true;
+            this.btnTopla.Click += new System.EventHandler(this.btnTopla_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(170, 185);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnCikar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 289);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnTopla);
+            this.Controls.Add(this.btnBol);
+            this.Controls.Add(this.btnCarp);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Button btnCarp;
+        private System.Windows.Forms.Button btnBol;
+        private System.Windows.Forms.Button btnTopla;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
